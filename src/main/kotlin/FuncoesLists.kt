13c8c44bc9069c8724b4bbc95132e44c8fa7f985
FelipeIgansi <lipeigansi@gmail.com>
2023-02-28 15:29:46 -0300

    Aula Listas - Distinct, sorted e reversed

--- src/main/kotlin/FuncoesLists.kt
+++ src/main/kotlin/FuncoesLists.kt
@@ -9,56 +9,74 @@ fun main() {
     // ----------------------------------------------------
 
     // First e Last
-/*
-    println("Primeira Receita:  ${data.first().nome}")
-    println("Ultima Receita:  ${data.last().nome}")
+    /*
+        println("Primeira Receita:  ${data.first().nome}")
+        println("Ultima Receita:  ${data.last().nome}")
 
-    println( listOf<Int>().firstOrNull())// Se a lista for vazia tem que usar o first or null
-    println( listOf<Int>().lastOrNull())// Se a lista for vazia tem que usar o last or null*/
+        println( listOf<Int>().firstOrNull())// Se a lista for vazia tem que usar o first or null
+        println( listOf<Int>().lastOrNull())// Se a lista for vazia tem que usar o last or null*/
 
     //----------------------------------------------------
 
-/*
-    // sum e sumOf
+    /*
+        // sum e sumOf
 
-    println(listOf(1,2,3,4,5).sum())
+        println(listOf(1,2,3,4,5).sum())
 
-    //Uso o sumOf para objetos basicamete
-    // it é como se fosse o this é uma referencia ao objeto
+        //Uso o sumOf para objetos basicamete
+        // it é como se fosse o this é uma referencia ao objeto
 
-    println(data.sumOf { it.calorias })
+        println(data.sumOf { it.calorias })
 
 
-    println(data.filter { it.calorias > 500 })
-    println("----------------------------------------------")
-    println(data.any { it.calorias > 500 })
-    println("----------------------------------------------")
-    println(data.count { it.calorias > 500 })
-*/
+        println(data.filter { it.calorias > 500 })
+        println("----------------------------------------------")
+        println(data.any { it.calorias > 500 })
+        println("----------------------------------------------")
+        println(data.count { it.calorias > 500 })
+    */
 
     // take e takelast
 //    println(data.take(2))// Quero as duas primeiras receitas
 //    println(data.takeLast(2))// Quero as duas Ultimas receitas
 
-/*    // foreach
-    data.forEach{ println(it.nome)}
-    println("-----------------------------------------")
-    data.filter { it.calorias >500 }.forEach{ println(it.nome) }
-        data.filter { it.calorias >500 }.forEach{ println("Nome:  ${it.nome}, Caloria:  ${it.calorias}, Ingredientes:  ${it.ingredientes}") }
+    /*    // foreach
+        data.forEach{ println(it.nome)}
+        println("-----------------------------------------")
+        data.filter { it.calorias >500 }.forEach{ println(it.nome) }
+            data.filter { it.calorias >500 }.forEach{ println("Nome:  ${it.nome}, Caloria:  ${it.calorias}, Ingredientes:  ${it.ingredientes}") }
 
 
-    */
+        */
 
-/*    // Max e Min
-    println(data.maxOf { it.calorias })
-    println(data.minOf { it.calorias })
+    /*    // Max e Min
+        println(data.maxOf { it.calorias })
+        println(data.minOf { it.calorias })
 
-    // Para mostrar tbm o objeto deve usar o By
+        // Para mostrar tbm o objeto deve usar o By
 
-    println(data.minByOrNull { it.calorias })*/
+        println(data.minByOrNull { it.calorias })*/
+
+    // Map
+    // Mapeia objetos em uma lista
+
+//    println(data.map { it.nome })
+
+    // output: [Lasanha, Panqueca, Omelete, Parmegiana, Sopa de feijões, Hamburguer]
 
 
+    // Avarage
+    /*
+        println(listOf(1,2,3,4).average())
+        println(data.map { it.calorias })
+        println(data.map { it.calorias }.average())*/
+    val list = listOf(1, 2, 3, 4, 1, 1, 1, 1, 5)
+//    println(list.distinct())// Mostra apenas os valores uma vez idependente do tipo
+//    data.distinctBy { it.nome }.forEach{ println( it.nome)}// faz a distinção por nome e só exibe os nomes
 
+    println("Lista ordenada: ${list.sorted()}")
+    println("Lista ordenada decrescente: ${list.sortedDescending()}")
+    println("Lista invertida: ${list.sortedDescending()}")
 
 }
 
@@ -74,6 +92,7 @@ fun geraDados(): List<Receita> {
             )
         ),
         Receita("Panqueca", 5000),
+        Receita("Panqueca", 1000),
         Receita("Omelete", 2000),
         Receita("Parmegiana", 700),
         Receita("Sopa de feijões", 300),
